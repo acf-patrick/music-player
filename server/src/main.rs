eac409008b2f1dec57b274febfe35c311bfe3d8d
acf-patrick <miharisoap@gmail.com>
2023-04-24 16:38:03 +0300

    server: scan folders

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,14 +3,34 @@
     windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+use std::fs;
+
+fn scan_audio_files(src_path: &str) -> Vec<String> {
+    let extensions = vec!["mp3", "m4a", "mp4", "flac", "wav"];
+    let mut list: Vec<String> = vec![];
+
+    for content in fs::read_dir(src_path).unwrap() {
+        if let Ok(entry) = content {
+            if let Ok(entry_type) = entry.file_type() {
+                if entry_type.is_dir() {
+                    let path = scan_audio_files(entry.path().to_str().unwrap());
+                    list.extend(path);
+                } else if entry_type.is_file() {
+                    if let Some(extension) = entry.path().extension() {
+                        if extensions.contains(&extension.to_str().unwrap()) {
+                            list.push(entry.path().to_str().unwrap().to_string());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return list;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
